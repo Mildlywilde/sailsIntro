c53dced4f0c42dc083501d1cdf5cc071aed545b6
Jack WIlde <jackwilde630@gmail.com>
2016-10-31 16:32:37 -0400

    build simple findOne action for person controller

---
+++ api/controllers/PersonController.js
@@ -0,0 +1,16 @@
+/**
+ * PersonController
+ *
+ * @description :: Server-side logic for managing people
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+	findOne: function (request, response) {
+		Person.find(request.params.id).exec(function (error, persons) {
+			var person = persons[0];
+			person.fullName = peson.firstName + ' ' + person.lastName;
+			response.json(person);
+		});
+	}
+};
